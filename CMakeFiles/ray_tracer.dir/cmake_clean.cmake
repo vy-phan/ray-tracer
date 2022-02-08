b7069d3df29f408cb020a1c28834f853991a9d1e
Vy Phan <nalu.phan@gmail.com>
2022-02-07 20:51:56 -0500

    Initialize Project

---
+++ CMakeFiles/ray_tracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray_tracer.dir/main.cpp.o"
+  "CMakeFiles/ray_tracer.dir/main.cpp.o.d"
+  "ray_tracer"
+  "ray_tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray_tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
