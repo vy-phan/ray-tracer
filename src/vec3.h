953e2920de4f1968034ed25b004251f2be1ebf02
Vy Phan <nalu.phan@gmail.com>
2022-02-08 20:36:29 -0500

    Added constructors in vec3.h

--- src/vec3.h
+++ src/vec3.h
@@ -1,8 +1,24 @@
-//
-// Created by Vy Phan on 2/7/22.
-//
+#ifndef VEC3_H
+#define VEC3_H
 
-#ifndef RAY_TRACER_VEC3_H
-#define RAY_TRACER_VEC3_H
+#include <cmath>
+#include <iostream>
 
-#endif //RAY_TRACER_VEC3_H
+using std::sqrt;
+
+class vec3 {
+public:
+    vec3() : e{ 0,0,0 } {}
+    vec3( double e0, double e1, double e2 ) : e{ e0, e1, e2 } {}
+
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3( -e[0], -e[1], -e[2] ); }
+
+
+
+};
+
+#endif
