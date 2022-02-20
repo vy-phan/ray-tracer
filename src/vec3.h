ffca489a41b22c4f4090085334a54aa0f29cae26
Vy Phan <nalu.phan@gmail.com>
2022-02-19 23:20:49 -0500

    Added vec3 Utility functions

--- src/vec3.h
+++ src/vec3.h
@@ -55,3 +55,50 @@ using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
 
 #endif
+
+
+// vec3 Utility Functions
+
+inline std::ostream& operator<<( std::ostream &out, const vec3 &v ) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+( const vec3 &u, const vec3 &v ) {
+    return vec3( u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2] );
+}
+
+inline vec3 operator-( const vec3 &u, const vec3 &v ) {
+    return vec3( u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2] );
+}
+
+inline vec3 operator*( const vec3 &u, const vec3 &v ) {
+    return vec3( u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2] );
+}
+
+inline vec3 operator*( double t, const vec3 &u ) {
+    return vec3( t * u.e[0], t * u.e[1], t * u.e[2] );
+}
+
+inline vec3 operator*( const vec3 &u, double t ) {
+    return t * u;
+}
+
+inline vec3 operator/( const vec3 &u, double t ) {
+    return ( 1 / t ) * u;
+}
+
+inline double dot( const vec3 &u, const vec3 &v ) {
+    return u.e[0] + v.e[0]
+         + u.e[1] + v.e[1]
+         + u.e[2] + v.e[2];
+}
+
+inline vec3 cross( const vec3 &u, const vec3 &v ) {
+    return vec3( u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                 u.e[0] * v.e[1] - u.e[1] * v.e[0] );
+}
+
+inline vec3 unit_vector( vec3 v ) {
+    return v / v.length();
+}
